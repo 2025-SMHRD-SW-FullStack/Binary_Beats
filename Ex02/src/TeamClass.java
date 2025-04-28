2f785a9c38210761a746631d1658fb6857fa1da7
JG <wprkf1005@gmail.com>
2025-04-28 15:18:46 +0900

    커밋

---
+++ Ex02/src/TeamClass.java
@@ -0,0 +1,16 @@
+
+public class TeamClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("팀장 첫번째");
+		System.out.println("팀원 1 두번째");
+		System.out.println("아아아아아아아아");
+		System.out.println("홍성재");
+		System.out.println("test2");
+		System.out.println(" rkhr"); 
+		System.out.println("희돈바보");
+
+	}
+
+}
